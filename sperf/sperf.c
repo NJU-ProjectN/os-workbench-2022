0046d3ab88374e99c3899d356dd9714fb801a28b
Yanyan Jiang <jiangyy@outlook.com>
2022-03-24 05:53:37 +0000

    M3 - sperf

---
+++ sperf/sperf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  char *exec_argv[] = { "strace", "ls", NULL, };
+  char *exec_envp[] = { "PATH=/bin", NULL, };
+  execve("strace",          exec_argv, exec_envp);
+  execve("/bin/strace",     exec_argv, exec_envp);
+  execve("/usr/bin/strace", exec_argv, exec_envp);
+  perror(argv[0]);
+  exit(EXIT_FAILURE);
+}
