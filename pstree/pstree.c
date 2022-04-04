e46bf11a7e47a550dc78d454e789114eb2aa85b4
tracer-nju <tracer@nju.edu.cn>
2022-04-04 20:59:12 +0800

    Linux VM-24-4-debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  20:59:12 up 27 min,  0 users,  load average: 0.09, 0.06, 0.01

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,16 +1,64 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+struct Args {
+  int show_pids;
+  int numeric_sort;
+  int version;
+};
+
+void print_args(int argc, char *argv[]){
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+}
+
+int process_args(int argc, char *argv[], struct Args *args){
+  for (int i = 0; i < argc; i++) {
+    char* argv_i = argv[i];
+    if (strcmp("-p", argv_i) == 0 || strcmp("--show-pids", argv_i) == 0){
+      args->show_pids = 1;
+    } else if (strcmp("-n", argv_i) == 0 || strcmp("--numeric-sort", argv_i) == 0){
+      args->numeric_sort = 1;
+    } else if (strcmp("-V", argv_i) == 0 || strcmp("--version", argv_i) == 0) {
+      args->version = 1;
+    } else {
+      printf("invalid option -- '%s'", argv);
+      return -1;
+    }
+  }
+  return 0;
+}
+
+void print_usage(){
+  printf("Usage: pstree [-gn]");
+  printf("pstree -v"); 
+}
+
+int main(int argc, char *argv[]) {
+  // print args;
+  print_args(argc, argv);
   // process parameter;
+  struct Args args;
+  args.numeric_sort = 0;
+  args.show_pids = 0;
+  args.version = 0;
+
+  int error_code = process_args(argc, argv, &args);
+  if (error_code != 0) {
+    print_usage();
+    return -1;
+  }
+
   // get pid list;
   // construct tree, find the root of each pid, and construt the tree;
   // print the pid tree;
 
   return 0;
 }
+
+
+
