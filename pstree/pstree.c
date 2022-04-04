155e0b875b616abb651755fe0e87099aa1b79ac2
tracer-nju <tracer@nju.edu.cn>
2022-04-04 20:57:54 +0800

    Linux VM-24-4-debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  20:57:54 up 25 min,  0 users,  load average: 0.05, 0.05, 0.02

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -7,5 +7,10 @@ int main(int argc, char *argv[]) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+  // process parameter;
+  // get pid list;
+  // construct tree, find the root of each pid, and construt the tree;
+  // print the pid tree;
+
   return 0;
 }
