bb4e9f2d4ec8ebcb60bd1e4d25421c7926175158
tracer-nju <tracer@nju.edu.cn>
2022-04-04 21:29:40 +0800

    Linux VM-24-4-debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  21:29:40 up 57 min,  0 users,  load average: 0.00, 0.00, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -103,6 +103,9 @@ int main(int argc, char *argv[]) {
   int pid_num;
   error_code = get_pid_list(&pid_list, &pid_num);
   if(error_code == 0){
+    if (pid_list != NULL) {
+      free(pid_list);
+    }
     return -1;
   }
 
