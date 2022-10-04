8969187c1c0dfbec40266ba6ea7b390aded97402
tracer-nju <tracer@nju.edu.cn>
2022-10-04 23:24:19 +0800

    Linux gongxing-lenovo 5.4.0-126-generic #142~18.04.1-Ubuntu SMP Thu Sep 1 16:25:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:24:19 up 12 days,  5:55,  1 user,  load average: 0.40, 0.33, 0.29

--- libco/co.c
+++ libco/co.c
@@ -83,7 +83,7 @@ static CoPool* co_remove(CoPool* remove_node) {
 static inline void stack_switch_call(void* sp, void *entry, void* arg) {
   asm volatile(
 #if __x86_64__
-    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+    "movq %0, %%rsp; movq %2, %%rdi; callq *%1"
     ::"b"((uintptr_t)sp - 1024), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
 #else
     "movl %0, %%rsp; movl %2, 4(%0); call *%1"
