a9d83140443cf9b96cfb4a694859cade12119e6e
tracer-nju <tracer@nju.edu.cn>
2022-10-05 12:08:25 +0800

    Linux gongxing-lenovo 5.4.0-126-generic #142~18.04.1-Ubuntu SMP Thu Sep 1 16:25:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:08:25 up 12 days, 18:39,  1 user,  load average: 0.38, 0.37, 0.43

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -122,8 +122,8 @@ static void test_2() {
 int main() {
     setbuf(stdout, NULL);
 
-    /** printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n"); */
-    /** test_1(); */
+    printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
+    test_1();
 
     printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
     test_2();
