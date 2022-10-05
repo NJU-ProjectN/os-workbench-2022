a3c881aa34f35e2def5f9211f784c349c81029bd
tracer-nju <tracer@nju.edu.cn>
2022-10-05 11:49:30 +0800

    Linux gongxing-lenovo 5.4.0-126-generic #142~18.04.1-Ubuntu SMP Thu Sep 1 16:25:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:49:30 up 12 days, 18:21,  1 user,  load average: 0.34, 0.49, 0.64

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -19,7 +19,6 @@ static int get_count() {
 
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
-    assert(arg);
     for (int i = 0; i < 100; ++i) {
       printf("%s%d  ", s, get_count());
       add_count();
