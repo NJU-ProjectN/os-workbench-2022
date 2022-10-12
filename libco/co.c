23d26589b4b8288217f9f82d15c4a125e7e3a804
tracer-nju <tracer@nju.edu.cn>
2022-10-12 10:01:20 -0700

    Linux ubuntu 5.4.0-128-generic #144~18.04.1-Ubuntu SMP Thu Sep 22 11:08:41 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:01:20 up  1:43,  1 user,  load average: 0.70, 0.54, 0.39

--- libco/co.c
+++ libco/co.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #define STACK_SIZE 20480
+#define ADDR_SIZE sizeof(void *)
 
 enum co_status {
   CO_NEW = 0,
@@ -102,10 +103,10 @@ void schedule() {
     g_running_co = co_to_run;
     if (co_to_run->status_ == CO_NEW) {
       co_to_run->status_ = CO_RUNNING;
-      memcpy(co_to_run->stack_ + 0x4fe8, (&co_to_run->exit_func),
-             sizeof(void *));
-      stack_switch_call(co_to_run->stack_ + 0x4fe8, co_to_run->func_,
-                        (uintptr_t)co_to_run->arg_);
+      memcpy(co_to_run->stack_ + STACK_SIZE - ADDR_SIZE,
+             (&co_to_run->exit_func), ADDR_SIZE);
+      stack_switch_call(co_to_run->stack_ + STACK_SIZE - ADDR_SIZE,
+                        co_to_run->func_, (uintptr_t)co_to_run->arg_);
     } else {
       co_to_run->status_ = CO_RUNNING;
       longjmp(co_to_run->context_, 1);
