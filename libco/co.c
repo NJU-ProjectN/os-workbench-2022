e892f075f969bb4b185a07408fc3cc5a74759ed4
tracer-nju <tracer@nju.edu.cn>
2022-10-12 10:51:09 -0700

    Linux ubuntu 5.4.0-128-generic #144~18.04.1-Ubuntu SMP Thu Sep 22 11:08:41 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:51:09 up  2:33,  1 user,  load average: 0.49, 0.29, 0.32

--- libco/co.c
+++ libco/co.c
@@ -114,7 +114,6 @@ void co_exit() {
   struct co *co_self = g_running_co;
   struct co *waiter = co_self->waiter_;
   if (waiter != NULL) {
-    assert(waiter == RemoveFromList(waiting_list_guard, waiter->name_));
     // wake waiter
     InsertToList(sched_list_guard, waiter);
     g_sched_list_size++;
