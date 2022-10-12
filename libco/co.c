5869a29c5d90b2db03a748feb58270272f9f2c29
tracer-nju <tracer@nju.edu.cn>
2022-10-12 09:35:45 -0700

    Linux ubuntu 5.4.0-128-generic #144~18.04.1-Ubuntu SMP Thu Sep 22 11:08:41 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:35:45 up  1:18,  1 user,  load average: 0.52, 0.59, 0.44

--- libco/co.c
+++ libco/co.c
@@ -102,8 +102,7 @@ void schedule() {
     g_running_co = co_to_run;
     if (co_to_run->status_ == CO_NEW) {
       co_to_run->status_ = CO_RUNNING;
-      memset(co_to_run->stack_ + 0x4ff8, (uintptr_t)co_to_run->exit_func,
-             sizeof(uintptr_t));
+      memset(co_to_run->stack_ + 0x4ff8, co_to_run->exit_func, sizeof(void *));
       stack_switch_call(co_to_run->stack_ + 0x4ff8, co_to_run->func_,
                         (uintptr_t)co_to_run->arg_);
     } else {
